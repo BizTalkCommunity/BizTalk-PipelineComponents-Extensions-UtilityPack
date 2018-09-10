bbe7537f489128c924580e4cef374cca87bcbb4a
Sandro Pereira <sandro-pereira@live.com.pt>
2018-09-10 16:02:01 +0100

    First version of the project

---
+++ BTS2016/BizTalk.PipelineComponents.CBROperationPromotionDecode/CBROperationPromotionDecode.cs
@@ -0,0 +1,219 @@
+namespace PipelineComponents.OperationPromotion
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Drawing;
+    using System.Resources;
+    using System.Reflection;
+    using System.Diagnostics;
+    using System.Collections;
+    using System.ComponentModel;
+    using Microsoft.BizTalk.Message.Interop;
+    using Microsoft.BizTalk.Component.Interop;
+    using Microsoft.BizTalk.Component;
+    using Microsoft.BizTalk.Messaging;
+    using Microsoft.BizTalk.Streaming;
+    
+    
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [System.Runtime.InteropServices.Guid("f286b184-1936-488f-9697-5161b1a4a204")]
+    [ComponentCategory(CategoryTypes.CATID_Encoder)]
+    public class CBROperationPromotionDecode : Microsoft.BizTalk.Component.Interop.IComponent, IBaseComponent, IPersistPropertyBag, IComponentUI
+    {
+        
+        private System.Resources.ResourceManager resourceManager = new System.Resources.ResourceManager("PipelineComponents.OperationPromotion.CBROperationPromotionDecode", Assembly.GetExecutingAssembly());
+        
+        #region IBaseComponent members
+        /// <summary>
+        /// Name of the component
+        /// </summary>
+        [Browsable(false)]
+        public string Name
+        {
+            get
+            {
+                return resourceManager.GetString("COMPONENTNAME", System.Globalization.CultureInfo.InvariantCulture);
+            }
+        }
+        
+        /// <summary>
+        /// Version of the component
+        /// </summary>
+        [Browsable(false)]
+        public string Version
+        {
+            get
+            {
+                return resourceManager.GetString("COMPONENTVERSION", System.Globalization.CultureInfo.InvariantCulture);
+            }
+        }
+        
+        /// <summary>
+        /// Description of the component
+        /// </summary>
+        [Browsable(false)]
+        public string Description
+        {
+            get
+            {
+                return resourceManager.GetString("COMPONENTDESCRIPTION", System.Globalization.CultureInfo.InvariantCulture);
+            }
+        }
+        #endregion
+        
+        #region IPersistPropertyBag members
+        /// <summary>
+        /// Gets class ID of component for usage from unmanaged code.
+        /// </summary>
+        /// <param name="classid">
+        /// Class ID of the component
+        /// </param>
+        public void GetClassID(out System.Guid classid)
+        {
+            classid = new System.Guid("f286b184-1936-488f-9697-5161b1a4a204");
+        }
+        
+        /// <summary>
+        /// not implemented
+        /// </summary>
+        public void InitNew()
+        {
+        }
+        
+        /// <summary>
+        /// Loads configuration properties for the component
+        /// </summary>
+        /// <param name="pb">Configuration property bag</param>
+        /// <param name="errlog">Error status</param>
+        public virtual void Load(Microsoft.BizTalk.Component.Interop.IPropertyBag pb, int errlog)
+        {
+        }
+        
+        /// <summary>
+        /// Saves the current component configuration into the property bag
+        /// </summary>
+        /// <param name="pb">Configuration property bag</param>
+        /// <param name="fClearDirty">not used</param>
+        /// <param name="fSaveAllProperties">not used</param>
+        public virtual void Save(Microsoft.BizTalk.Component.Interop.IPropertyBag pb, bool fClearDirty, bool fSaveAllProperties)
+        {
+        }
+        
+        #region utility functionality
+        /// <summary>
+        /// Reads property value from property bag
+        /// </summary>
+        /// <param name="pb">Property bag</param>
+        /// <param name="propName">Name of property</param>
+        /// <returns>Value of the property</returns>
+        private object ReadPropertyBag(Microsoft.BizTalk.Component.Interop.IPropertyBag pb, string propName)
+        {
+            object val = null;
+            try
+            {
+                pb.Read(propName, out val, 0);
+            }
+            catch (System.ArgumentException )
+            {
+                return val;
+            }
+            catch (System.Exception e)
+            {
+                throw new System.ApplicationException(e.Message);
+            }
+            return val;
+        }
+        
+        /// <summary>
+        /// Writes property values into a property bag.
+        /// </summary>
+        /// <param name="pb">Property bag.</param>
+        /// <param name="propName">Name of property.</param>
+        /// <param name="val">Value of property.</param>
+        private void WritePropertyBag(Microsoft.BizTalk.Component.Interop.IPropertyBag pb, string propName, object val)
+        {
+            try
+            {
+                pb.Write(propName, ref val);
+            }
+            catch (System.Exception e)
+            {
+                throw new System.ApplicationException(e.Message);
+            }
+        }
+        #endregion
+        #endregion
+        
+        #region IComponentUI members
+        /// <summary>
+        /// Component icon to use in BizTalk Editor
+        /// </summary>
+        [Browsable(false)]
+        public IntPtr Icon
+        {
+            get
+            {
+                return ((System.Drawing.Bitmap)(this.resourceManager.GetObject("COMPONENTICON", System.Globalization.CultureInfo.InvariantCulture))).GetHicon();
+            }
+        }
+        
+        /// <summary>
+        /// The Validate method is called by the BizTalk Editor during the build 
+        /// of a BizTalk project.
+        /// </summary>
+        /// <param name="obj">An Object containing the configuration properties.</param>
+        /// <returns>The IEnumerator enables the caller to enumerate through a collection of strings containing error messages. These error messages appear as compiler error messages. To report successful property validation, the method should return an empty enumerator.</returns>
+        public System.Collections.IEnumerator Validate(object obj)
+        {
+            // example implementation:
+            // ArrayList errorList = new ArrayList();
+            // errorList.Add("This is a compiler error");
+            // return errorList.GetEnumerator();
+            return null;
+        }
+        #endregion
+        
+        #region IComponent members
+        /// <summary>
+        /// Implements IComponent.Execute method.
+        /// </summary>
+        /// <param name="pc">Pipeline context</param>
+        /// <param name="inmsg">Input message</param>
+        /// <returns>Original input message</returns>
+        /// <remarks>
+        /// IComponent.Execute method is used to initiate
+        /// the processing of the message in this pipeline component.
+        /// </remarks>
+        public Microsoft.BizTalk.Message.Interop.IBaseMessage Execute(Microsoft.BizTalk.Component.Interop.IPipelineContext pc, 
+            Microsoft.BizTalk.Message.Interop.IBaseMessage inmsg)
+        {
+            /*************************************************************************************************************************
+             * This component will read the MessageType from the Message Context Properties.                                         *
+             *     BTS.MessageType: Specifies the type of the message. The message type is defined as a concatenation of document    *
+             *                      schema namespace and document root node: http://<MyNamespace>#<MyRootNode>.                      *
+             * and will promote the Operation property based on the content on the Root node "<MyRootNode>" specified on the         *
+             * message type "http://<MyNamespace>#<MyRootNode>" of the message.                                                      *
+             *                                                                                                                       *
+             * Ex: Message Type = "http://schemas.integration.com#GetClientInfo"                                                     *
+             *     Operation will be defined as "GetClientInfo"                                                                      *
+             ************************************************************************************************************************/
+
+            // Note:
+            // System properties are mostly used internally by BizTalk Messaging Engine and its components. 
+            // In general, changing the values set by the engine for those properties is not recommended, because it may affect 
+            // the execution logic of the engine. However, there are a large number of properties that you can change. 
+
+            string msgType = Convert.ToString(inmsg.Context.Read("MessageType", 
+                                                                 "http://schemas.microsoft.com/BizTalk/2003/system-properties"));
+
+            string operation = msgType.Substring(msgType.LastIndexOf('#') + 1);
+
+            inmsg.Context.Promote("Operation", "http://schemas.microsoft.com/BizTalk/2003/system-properties",
+                                  operation);
+
+            return inmsg;
+        }
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# BizTalk PipelineComponents Extensions UtilityPack
+BizTalk PipelineComponents Extensions UtilityPack is a set of custom pipeline components (libraries) with several custom pipeline components that can be used in received and sent pipelines, which will provide an extension of BizTalk out-of-the-box pipeline capabilities.
+
+## Content-Based Routing
+### CBRIdocOperationPromotionDecode
+
+* Content Based Routing Component to promote IDOC Operation property.
+* **This component requires one configuration that is the MessageType string to be ignored. Then it will take the last string (word) from the MessageType Message Context Property and promote it to the Operation Message Context Property.
+
+### CBROperationPromotionDecode
+
+* Content Based Routing Component to promote Operation property.
+* **This component doesn't requires any configuration. Then it will take the value (word) which lies ahead of the cardinal (#) from the MessageType message context property and promote it to the Operation Message Context Property.
