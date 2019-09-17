8f846d3c75f563a8bd4d78582c11f4b41c8e02d2
Sandro Pereira <sandro-pereira@live.com.pt>
2019-09-17 09:47:01 +0100

    Multi-Part Message Attachments Zipper Pipeline Component for BizTalk 2006/2009

---
+++ BTS2006-2009/Multi-Part-Message-Attachments-Zipper/C#/BizTalk06.PipelineComponent.MultiPartMsg.ZipAttach/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BizTalk06.PipelineComponent.MultiPartMsg.ZipAttach.csproj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Devscope")]
+[assembly: AssemblyProduct("BizTalk06.PipelineComponent.MultiPartMsg.ZipAttach.csproj")]
+[assembly: AssemblyCopyright("Copyright © Devscope 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50b90cb8-e7b0-40dc-b371-3724a29e9183")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
